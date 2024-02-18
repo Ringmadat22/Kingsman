7e854f0bfbfc79e2921baa3ba5a39ef845047233
Ring Madat <smithemma329@gmail.com>
2024-02-18 21:07:42 +0300

    main

--- assets/js/script.js
+++ assets/js/script.js
@@ -95,3 +95,65 @@ const scrollReveal = function () {
 scrollReveal();
 
 addEventOnElem(window, "scroll", scrollReveal);
+
+const rangeInput = document.querySelectorAll(".range-input input"),
+priceInput = document.querySelectorAll(".price-input input"),
+range = document.querySelector(".slider .progress");
+let priceGap = 1000;
+priceInput.forEach(input =>{
+    input.addEventListener("input", e =>{
+        let minPrice = parseInt(priceInput[0].value),
+        maxPrice = parseInt(priceInput[1].value);
+        
+        if((maxPrice - minPrice >= priceGap) && maxPrice <= rangeInput[1].max){
+            if(e.target.className === "input-min"){
+                rangeInput[0].value = minPrice;
+                range.style.left = ((minPrice / rangeInput[0].max) * 100) + "%";
+            }else{
+                rangeInput[1].value = maxPrice;
+                range.style.right = 100 - (maxPrice / rangeInput[1].max) * 100 + "%";
+            }
+        }
+    });
+});
+rangeInput.forEach(input =>{
+    input.addEventListener("input", e =>{
+        let minVal = parseInt(rangeInput[0].value),
+        maxVal = parseInt(rangeInput[1].value);
+        if((maxVal - minVal) < priceGap){
+            if(e.target.className === "range-min"){
+                rangeInput[0].value = maxVal - priceGap
+            }else{
+                rangeInput[1].value = minVal + priceGap;
+            }
+        }else{
+            priceInput[0].value = minVal;
+            priceInput[1].value = maxVal;
+            range.style.left = ((minVal / rangeInput[0].max) * 100) + "%";
+            range.style.right = 100 - (maxVal / rangeInput[1].max) * 100 + "%";
+        }
+    });
+});
+
+// Get the search input element and the list of items
+const searchInput = document.getElementById('search');
+const searchInput2 = document.getElementById('search');
+const itemList = document.getElementById('item-list');
+const itemList2 = document.getElementById('items-list');
+
+// Add an event listener to the search input for real-time filtering
+searchInput.addEventListener('input', filterItems);
+
+function filterItems() {
+    const searchQuery = searchInput.value.toLowerCase();
+    const items = itemList.getElementsByTagName('li');
+
+    for (const item of items) {
+        const itemText = item.textContent.toLowerCase();
+        if (itemText.includes(searchQuery)) {
+            item.style.display = 'block';
+        } else {
+            item.style.display = 'none';
+        }
+    }
+}
