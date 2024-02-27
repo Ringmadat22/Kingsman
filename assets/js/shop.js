669141ad4e5f392c4b851d4951b4c9dde47ab15c
Ring Madat <smithemma329@gmail.com>
2024-02-27 07:27:07 +0300

    main

--- assets/js/shop.js
+++ assets/js/shop.js
@@ -12,3 +12,32 @@
     });
   }
 
+  function filterProducts() {
+    var select = document.getElementById("sort-select");
+    var selectedOption = select.options[select.selectedIndex].value;
+
+    var products = document.querySelectorAll('.shop-item');
+
+    var sortedProducts = Array.from(products).sort(function(a, b) {
+        var priceA = parseFloat(a.querySelector('.price .span').textContent.replace('$', ''));
+        var priceB = parseFloat(b.querySelector('.price .span').textContent.replace('$', ''));
+
+        if (selectedOption === "highest") {
+            return priceB - priceA;
+        } else if (selectedOption === "lowest") {
+            return priceA - priceB;
+        } else {
+            // Do nothing for other options or handle as needed
+            return 0;
+        }
+    });
+
+    // Clear the current product list
+    var productList = document.querySelector('.shop-ul');
+    productList.innerHTML = '';
+
+    // Append sorted products to the product list
+    sortedProducts.forEach(function(product) {
+        productList.appendChild(product);
+    });
+}
